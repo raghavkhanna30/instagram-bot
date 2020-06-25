77ae4cf9bebb4173f93bc3b8a13352d8f24e4daa
Raghav khanna <48377506+raghavkhanna30@users.noreply.github.com>
2020-06-25 14:25:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from instapy import InstaPy
 
-session = InstaPy(username="your username here...", password="your password here...", headless_browser=True)
+session = InstaPy(username="ENTER USERNAME", password="ENTER PASSWORD", headless_browser=True)
 
 #headless browser = true implies that it will work in background without opening browser
 session.login()
@@ -9,14 +9,14 @@ session.login()
 
 #like section (Chnage tags here)
 session.like_by_tags(['javascript', 'python'], amount=3)
-session.set_dont_like(["naked", "nsfw", "sex"])
+session.set_dont_like(["naked", "sex"])
 
 #following section 
 session.set_do_follow(True, percentage=50, times=1)
 
 #comment section (Add more comments)
 session.set_do_comment(True, percentage=100)
-session.set_comments([u'I love your post @{}!'])
+session.set_comments([u'I AM MADE BY @erRaghavkhanna @{}!'])
 
 
 session.end()
